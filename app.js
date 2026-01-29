99004da3f01f762133f373033faf1050af6b876c
Saloni Kashyap <kashyapsaloni68292@gmail.com>
2026-01-29 23:10:24 +0530

    Fix MongoDB env + connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-const express = require("express");
+console.log("ENV CHECK ATLASDB_URL =", process.env.ATLASDB_URL);
+
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
